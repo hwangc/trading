6ea110daf9db93f29501be86308775f6cbdb9313
HwangC <hwangc@hwangc.com>
2015-10-12 08:39:55 +0900

    a few changes
    variables

---
+++ server/startup.js
@@ -0,0 +1,48 @@
+// On server startup, if the database is empty, create some initial data.
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+
+    if (Trading.find().count() < 20) {
+
+      for (var i = 0; i < 20; i++) {
+
+        // var randomCat = function() {
+        var ranCat = '', ranTag = [], slug = '', name = '';
+
+        ranCat = Random.choice(['product', 'service']);
+
+        // select tag from tempTag
+        switch (ranCat) {
+          case "product":
+            ranTag.push(Random.choice(["product1", "product2", "product3", "product4", "product5", "product6", "product7", "product8", "product9", "product10"]));
+            break;
+          case "service":
+            ranTag.push(Random.choice(["service1", "service2", "service3", "service4", "service5", "service6", "service7", "service8", "service9", "service10"]));
+            break;
+
+          default:
+            console.error("no category selected");
+            break;
+        }
+
+        name = Fake.word();
+
+        slug = strToURL(name);
+
+        Trading.insert({
+          userID: Meteor.users.findOne()._id,
+          profileImg: Meteor.absoluteUrl("img/user-50x50.png"),
+          itemName: name,
+          itemDescription: Fake.sentence([5]),
+          category: ranCat,
+          tags: ranTag,
+          slug: slug,
+          bidderCount: 0,
+          progress: "unchecked",
+          createdAt: new Date(),
+          bidders: []
+        });
+      }
+    }
+  });
+}
